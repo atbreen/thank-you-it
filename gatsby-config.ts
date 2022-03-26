5e020adc4759719bff5c523c5076431a0b3d5fa4
aprilBreen <84913184+atbreen@users.noreply.github.com>
2022-03-26 19:24:09 -0400

    Path fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/thank-you-it",
   siteMetadata: {
     title: ``,
     siteUrl: `https://www.yourdomain.tld`,
